e1b35ceb521f75cc39e82d4ddb2068e90fb34e67
uranusnep <miruru211@outlook.jp>
2020-12-31 08:55:01 +0000

    12.31

--- isara.js
+++ isara.js
@@ -1,15 +1,14 @@
 $(function(){
     
     $(".panel-heading").click(function(){
-        $(this).next().slideToggle() 
+        $(this).next().slideToggle();
+        $(this).children("p").addClass("underline-class");
+        $(this).toggleClass("open")
     });
     
-    $(".panel-heading").click(function(){
-        $(this).children(".question-toggle").toggleClass("angle-up-toggle")
+    $(".panel-heading").on("click", function(){
+        $(".panel-heading").not(this).children("p").removeClass("underline-class")
     });
     
-    $(".panel-text").click(function(){
-        $(this).css("text-decoration","underline")
-    });
     
 });
