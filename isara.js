78662757a45f23fc2689beb05189686e06965c37
uranusnep <miruru211@outlook.jp>
2020-12-31 10:04:49 +0000

    12.31更新

--- isara.js
+++ isara.js
@@ -10,5 +10,30 @@ $(function(){
         $(".panel-heading").not(this).children("p").removeClass("underline-class")
     });
     
+    $(".pagetop").hide();
+    
+    $(window).scroll(function(){
+        if($(window).scrollTop()>0){
+            $(".pagetop").fadeIn();
+        }else{
+            $(".pagetop").fadeOut();
+        }
+    });
+    
+    $(".pagetop").click(function(){
+        $("html, body").animate({scrollTop:0},"slow");
+    });
+    
+    $(".btn").click(function(){
+        var down = $(".contactform").offset().top;
+        $("html, body").animate({scrollTop:down},"slow");
+    });
+    
+    $(".conversionbtn").click(function() {
+        var down = $(".contactform").offset().top;
+        $("html, body").animate({scrollTop:down},"slow");
+    });
+    
+    
     
 });
